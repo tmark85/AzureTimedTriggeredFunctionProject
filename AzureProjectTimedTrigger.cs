006177b46e9ffc8c070c62530701c8878901ee3f
tmark85 <41347308+tmark85@users.noreply.github.com>
2025-01-30 00:17:34 +0100

    Update AzureProjectTimedTrigger.cs

--- AzureProjectTimedTrigger.cs
+++ AzureProjectTimedTrigger.cs
@@ -1,59 +1,27 @@
 using System;
-using System.Text;
-using System.Net;
-using Newtonsoft.Json;
-using System.Threading.Tasks;
-using System.Net.Http;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
-namespace AzureProjectTimedTrigger.Function
+public static class AzureProjectTimedTrigger
 {
-    public static class AzureProjectTimedTrigger
+    [FunctionName("TimerTriggerFunction1")]
+    public static void Run1([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
     {
-        [FunctionName("AzureProjectTimedTrigger")]
-        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
-        {
-            var jsonString = await MakeStackOverflowRequest();
+        log.LogInformation($"Timer trigger function 1 executed at: {DateTime.Now}");
+        // Your function logic here
+    }
 
-            var jsonOb = JsonConvert.DeserializeObject<dynamic>(jsonString);
+    [FunctionName("TimerTriggerFunction2")]
+    public static void Run2([TimerTrigger("0 0 * * * *")] TimerInfo myTimer, ILogger log)
+    {
+        log.LogInformation($"Timer trigger function 2 executed at: {DateTime.Now}");
+        // Your function logic here
+    }
 
-            var newQuestionCount = jsonOb.items.Count;
-
-            await MakeSlackRequest($"You have {newQuestionCount} question on Stack Overflow!");
-
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-        }
-
-        public static async Task<string> MakeStackOverflowRequest()
-        {
-            HttpClientHandler handler = new HttpClientHandler()
-            {
-                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
-            };
-
-            using (var client = new HttpClient(handler))
-            {
-                var response = await client.GetAsync($"https://api.stackexchange.com/2.3/search?fromdate=1635206400&order=desc&sort=activity&intitle=azure&site=stackoverflow");
-
-                var result = await response.Content.ReadAsStringAsync();
-
-                return result;
-            }
-        }
-        public static async Task<string> MakeSlackRequest(string message)
-        {
-            using (var client = new HttpClient())
-            {
-                var requestData = new StringContent("{'text':'" + message + "'}", Encoding.UTF8, "application/json");
-
-                var response = await client.PostAsync($"EnteryourwebhookURLhere", requestData);
-
-                var result = await response.Content.ReadAsStringAsync();
-
-                return result;
-            }
-        }
+    [FunctionName("TimerTriggerFunction3")]
+    public static void Run3([TimerTrigger("0 0 12 * * *")] TimerInfo myTimer, ILogger log)
+    {
+        log.LogInformation($"Timer trigger function 3 executed at: {DateTime.Now}");
+        // Your function logic here
     }
 }
